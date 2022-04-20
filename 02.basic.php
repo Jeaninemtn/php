347b4f39e614db1bac9cf172df194c1c9f40ee18
Jeanine <jeaninemtn@gmail.com>
2022-04-20 16:40:53 +0800

    Initial code

---
+++ 02.basic.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basic php</title>
+</head>
+
+<body>
+<?php
+    echo 2e3;
+    echo '<br>';
+    echo __DIR__. '<br>';
+    echo __FILE__. '<br>';
+    echo __LINE__. '<br>';
+?>
+</body>
+
+</html>
