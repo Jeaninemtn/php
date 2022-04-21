35fa91e10a1010938df8e914e95d987a0051cab3
Jeanine <jeaninemtn@gmail.com>
2022-04-21 10:17:39 +0800

    Initial code

---
+++ 05.basic.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $a = "2a";
+    $b = 23;
+
+    echo $a + $b;
+    echo '<br>';
+
+    echo $a * $b;
+    echo '<br>';
+
+    echo "$a + $b <br>";
+    echo "\$a = $a <br>";
+    echo '$a + $b <br>';
+
+    // 單純字串 用單引號標示
+    // 如果是變數 用雙引號標示
+
+?>
+</body>
+</html>
