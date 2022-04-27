55569dc92fb0ad4aa7291f261b9874393a252066
Jeaninemtn <jeaninemtn@gmail.com>
2022-04-27 11:53:04 +0800

    Update

---
+++ mmmh63-php/proj/ab-delete.php
@@ -0,0 +1,18 @@
+<?php
+
+require './parts/connect-db.php';
+//echo $_SERVER['HTTP_REFERER'];
+//exit;
+
+$sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
+if (!empty($sid)) {
+    $pdo->query("DELETE FROM address_book WHERE sid=$sid");
+}
+
+$comeFrome = 'ab-list.php'; // 給預設值
+
+if(!empty($_SERVER['HTTP_REFERER'])) {
+    $comeFrome = $_SERVER['HTTP_REFERER'];
+}
+
+header('Location: '. $comeFrome);
