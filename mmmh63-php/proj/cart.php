bc92c87679a99ac56e98d5eab810fb0016595fb8
Jeaninemtn <jeaninemtn@gmail.com>
2022-05-03 14:53:00 +0800

    Update

--- mmmh63-php/proj/cart.php
+++ mmmh63-php/proj/cart.php
@@ -45,8 +45,11 @@ echo json_encode([
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($sids as $sid) :
+                    <?php
+                    $total = 0;
+                    foreach ($sids as $sid) :
                         $item = $rows[$sid];
+                        $total += $item['price'] * $item['quantity'];
                     ?>
                     <tr>
                         <td><?= $sid ?></td>
@@ -54,7 +57,7 @@ echo json_encode([
                             <img src="imgs/small/<?= $item['book_id'] ?>.jpg" alt="">
                         </td>
                         <td><?= $item['bookname'] ?></td>
-                        <td><?= $item['price'] ?></td>
+                        <td class="price" data-price="<?= $item['price'] ?>"></td>
                         <td>
                             <select class="form-select form-select-sm" style="display:inline-block; width:auto">
                                 <?php for ($i = 1; $i <= 20; $i++) : ?>
@@ -63,7 +66,7 @@ echo json_encode([
                                 <?php endfor; ?>
                             </select>
                         </td>
-                        <td><?= $item['price'] * $item['quantity'] ?></td>
+                        <td class="sub-total"></td>
 
                     </tr>
                     <?php endforeach; ?>
@@ -74,7 +77,36 @@ echo json_encode([
 
         </div>
     </div>
+    <div class="row">
+        <div class="col">
+            <div class="alert alert-primary" role="alert">
+                總計: <span id="total-price"></span>
+            </div>
+        </div>
+    </div>
 </div>
 <?php include __DIR__ . '/parts/scripts.php' ?>
-<script></script>
+<script>
+const dollarCommas = function(n) {
+    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+};
+
+const calcPrices = () => {
+    const trs = $('tbody > tr');
+
+    let totalPrice = 0;
+    trs.each(function() {
+        const tr = $(this);
+        const price = +tr.find('.price').attr('data-price');
+        tr.find('.price').text('$ ' + dollarCommas(price)); // 顯示單價
+        const quantity = +tr.find('select').val();
+        tr.find('.sub-total').text('$ ' + dollarCommas(price * quantity)); // 顯示小計
+        totalPrice += price * quantity;
+    });
+
+    $('#total-price').text('$ ' + dollarCommas(totalPrice));
+
+};
+calcPrices();
+</script>
 <?php include __DIR__ . '/parts/html-foot.php' ?>
